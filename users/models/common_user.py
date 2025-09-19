f1f7ebd5997ed739bceb7a59924f6b25502a49bd
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-19 20:28:24 -0300

    FEAT: Create common user model #3

--- users/models/common_user.py
+++ users/models/common_user.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class CommonUser(models.Model):
     user = models.OneToOneField(
-        User, on_delete=models.CASCADE, related_name='user_profile'
+        User, on_delete=models.CASCADE, related_name='common_profile'
     )
     nome = models.CharField(max_length=100, null=False, blank=False)
     telefone = models.CharField(max_length=11, null=False, blank=False)
