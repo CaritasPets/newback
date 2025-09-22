b695ca60343a6876a45eed479e9812f430d65e69
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-22 14:15:09 -0300

    FEAT: Create the permission to CRUD Pets #5

--- adocao/views/perdido_pet.py
+++ adocao/views/perdido_pet.py
@@ -1,7 +1,22 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from ..serializers import PetPerdidoSerializer
 from ..models import PetPerdido
+from ..permissions import IsOwnerOrReadOnly
 
 class PetPerdidoViewSet(ModelViewSet):
     queryset = PetPerdido.objects.all()
     serializer_class = PetPerdidoSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsAuthenticated]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            permission_classes = [IsOwnerOrReadOnly]
+        else:
+            permission_classes = []
+        
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
