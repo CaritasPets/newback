76c548e8cabad4e128fbd4822ec654985577556c
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-23 12:05:04 -0300

    FEAT: Connect the API with DATABASE PostgreeSQL #11

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -102,10 +104,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://caritaspets_db_4z01_user:cHR1XfikSAfZyDCGYSylSewgSQ3wKuOV@dpg-d39b2e8dl3ps73aobh0g-a.oregon-postgres.render.com/caritaspets_db_4z01"
+    )
 }
 
 
