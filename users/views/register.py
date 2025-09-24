c111756e9f59be6c8d08409a691793bf2f10a157
AndreRicardoSc <andrenarutto7@gmail.com>
2025-09-24 17:59:24 -0300

    FIX: Adding FormParsers

--- users/views/register.py
+++ users/views/register.py
@@ -1,12 +1,14 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from ..serializers import RegisterSerializer, ProfileSerializer
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
+    parser_classes = [MultiPartParser, FormParser] 
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
