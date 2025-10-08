245ea74acbf067da82efbf23281b528510575ebe
AndreRicardoSc <andrenarutto7@gmail.com>
2025-10-07 23:04:19 -0300

    FIX: Ready for use

--- config/settings.py
+++ config/settings.py
@@ -112,9 +112,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        "postgresql://caritaspets_db_4z01_user:cHR1XfikSAfZyDCGYSylSewgSQ3wKuOV@dpg-d39b2e8dl3ps73aobh0g-a.oregon-postgres.render.com/caritaspets_db_4z01"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
